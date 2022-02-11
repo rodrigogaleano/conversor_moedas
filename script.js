6870717b8e5ae27593ac96c4f0c9c4c083acad65
rodrigogaleano <rodrigo1galeano@gmail.com>
2022-02-11 16:00:51 -0400

    Validação da resposta

--- script.js
+++ script.js
@@ -5,31 +5,40 @@ var gbp = null;
 document.addEventListener('DOMContentLoaded', () => {
     var url = 'https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL,GBP-BRL'
     fetch(url)
-    .then((res) => {
-        return res.json()
-    })
-    .then((data) => {
-        usd = data.USDBRL.bid;
-        eur = data.EURBRL.bid;
-        gbp = data.GBPBRL.bid;
-    })
+        .then((res) => {
+            return res.json()
+        })
+        .then((data) => {
+            usd = data.USDBRL.bid;
+            eur = data.EURBRL.bid;
+            gbp = data.GBPBRL.bid;
+        })
 })
 
 
 
 function converter() {
-
     let resDolar = document.querySelector('#dolar');
     let resEur = document.querySelector('#euro');
     let resGbp = document.querySelector('#libra');
     let container = document.querySelector('.resultado')
+    let erro = document.querySelector('.erro')
 
     let input = document.getElementById('valor');
     let valor = input.value;
 
-    container.style.display = 'flex';
-    resDolar.innerHTML = `<h3>U$${(valor / usd).toFixed(2)}</h3><smaller>Dólar</smaller>`;
-    resEur.innerHTML = `<h3>€${(valor / eur).toFixed(2)}</h3><smaller>Euro</smaller>`;
-    resGbp.innerHTML = `<h3>£${(valor / gbp).toFixed(2)}</h3><smaller>Libra</smaller>`;
+    console.log(valor);
 
+    if (valor == '') {
+        erro.style.display = 'flex';
+        container.style.display = 'none';
+        erro.innerHTML = 'Informe um valor válido para conversão!'
+    } else {
+        erro.style.display = 'none';
+        container.style.display = 'flex';
+        
+        resDolar.innerHTML = `<h3>U$${(valor / usd).toFixed(2)}</h3><smaller>Dólar</smaller>`;
+        resEur.innerHTML = `<h3>€${(valor / eur).toFixed(2)}</h3><smaller>Euro</smaller>`;
+        resGbp.innerHTML = `<h3>£${(valor / gbp).toFixed(2)}</h3><smaller>Libra</smaller>`;
+    }
 }
