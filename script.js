5d480a776e90f1d35c765b00369b30c147b61fbf
rodrigogaleano <rodrigo1galeano@gmail.com>
2022-02-11 00:04:45 -0400

    Adição euro e libra

--- script.js
+++ script.js
@@ -1,16 +1,33 @@
-let url = 'https://economia.awesomeapi.com.br/last/USD-BRL';
+var usd = null;
+var eur = null;
+var gbp = null;
+
+document.addEventListener('DOMContentLoaded', () => {
+    var url = 'https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL,GBP-BRL'
+    fetch(url)
+    .then((res) => {
+        return res.json()
+    })
+    .then((data) => {
+        usd = data.USDBRL.bid;
+        eur = data.EURBRL.bid;
+        gbp = data.GBPBRL.bid;
+    })
+})
+
+
 
 function converter() {
+
+    let resDolar = document.querySelector('#dolar');
+    let resEur = document.querySelector('#euro');
+    let resGbp = document.querySelector('#libra');
+
     let input = document.getElementById('valor');
     let valor = input.value;
 
-    fetch(url)
-        .then((res) => {
-            return res.json()
-        })
-        .then((data) => {
-            let rate = data.USDBRL.bid;
-            let resultado = `${valor}USD = ${rate * valor}BRL`;
-            document.getElementById('resultado').innerHTML = resultado;
-        })
+    resDolar.innerHTML = `Dólar: ${(valor / usd).toFixed(2)}`;
+    resEur.innerHTML = `Euro:  ${(valor / eur).toFixed(2)}`;
+    resGbp.innerHTML = `Libra: ${(valor / gbp).toFixed(2)}`;
+
 }
