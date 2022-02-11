b5eeb89a81a7ba9f695c9111e5e0df6049b51e59
rodrigogaleano <rodrigo1galeano@gmail.com>
2022-02-11 14:36:48 -0400

    Ajustes no layout

--- script.js
+++ script.js
@@ -22,12 +22,14 @@ function converter() {
     let resDolar = document.querySelector('#dolar');
     let resEur = document.querySelector('#euro');
     let resGbp = document.querySelector('#libra');
+    let container = document.querySelector('.resultado')
 
     let input = document.getElementById('valor');
     let valor = input.value;
 
-    resDolar.innerHTML = `Dólar: ${(valor / usd).toFixed(2)}`;
-    resEur.innerHTML = `Euro:  ${(valor / eur).toFixed(2)}`;
-    resGbp.innerHTML = `Libra: ${(valor / gbp).toFixed(2)}`;
+    container.style.display = 'flex';
+    resDolar.innerHTML = `<h3>U$${(valor / usd).toFixed(2)}</h3><smaller>Dólar</smaller>`;
+    resEur.innerHTML = `<h3>€${(valor / eur).toFixed(2)}</h3><smaller>Euro</smaller>`;
+    resGbp.innerHTML = `<h3>£${(valor / gbp).toFixed(2)}</h3><smaller>Libra</smaller>`;
 
 }
